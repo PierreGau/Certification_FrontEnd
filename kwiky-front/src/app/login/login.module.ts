ced3bb9bcc460739febd9e6b9ce577b0484b253d
PierreGau <p.gauthier09@gmail.com>
2022-12-21 14:39:48 +0100

    module login + page sign in

---
+++ kwiky-front/src/app/login/login.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
+
+
+@NgModule({
+  declarations: [
+    PageSignInComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }

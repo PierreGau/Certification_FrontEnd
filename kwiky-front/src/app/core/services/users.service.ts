279a60c90150d611bac952b417375d265ced263e
PierreGau <p.gauthier09@gmail.com>
2022-12-21 15:28:35 +0100

    UserService

--- kwiky-front/src/app/core/services/users.service.ts
+++ kwiky-front/src/app/core/services/users.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment/environment';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  private urlApi: string;
+  public users$: Observable<User[]>;
+  private userId: number;
+  public currentUser$: Observable<User>;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.urlApi = environment.urlApi;
+    this.users$ = this.httpClient.get<User[]>(`${this.urlApi}/users`);
+    this.userId = 1;
+    this.currentUser$ = this.httpClient.get<User>(`${this.urlApi}/user/${this.userId}`);
+  }
+
+  public changeUser(newUser: number) {
+    this.userId = newUser;
+  }
 }

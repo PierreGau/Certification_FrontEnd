b585749097260f40ed8aef87ffac418948033593
PierreGau <p.gauthier09@gmail.com>
2022-12-21 12:06:48 +0100

    ajout d'une ui récupérée sur le crm

---
+++ kwiky-front/src/app/ui/components/ui/ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.scss'],
+})
+export class UiComponent {
+  public close: boolean = false;
+
+  constructor() {}
+
+  public toggle(): void {
+    this.close = !this.close;
+  }
+}

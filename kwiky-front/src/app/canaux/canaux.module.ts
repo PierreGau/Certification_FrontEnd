7153b347a13b477fa85b1ef21355a60318e4faf5
sofiane <sofiane.mehali@outlook.com>
2022-12-21 11:22:17 +0100

    Ajout des modules métiers

---
+++ kwiky-front/src/app/canaux/canaux.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CanauxRoutingModule } from './canaux-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CanauxRoutingModule
+  ]
+})
+export class CanauxModule { }

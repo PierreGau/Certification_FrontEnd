b40082f35fea7370ea734af6864791d00c1e81a2
sofiane <sofiane.mehali@outlook.com>
2022-12-22 11:27:43 +0100

    Affichage d'une liste de message dans page-list-messages

--- kwiky-front/src/app/core/services/canaux.service.ts
+++ kwiky-front/src/app/core/services/canaux.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environment/environment';
+import { Canal } from '../models/canal';
+import { Message } from '../models/message';
+import { UsersService } from './users.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CanauxService {
+  private urlApi: string;
+  public myChannels$: Observable<Canal[]>;
+  public channels$: Observable<Canal[]>;
+  public selectedChannel: Subject<Canal>;
+  public listMessages$: Observable<Message[]>;
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private usersService: UsersService
+  ) {
+    this.urlApi = environment.urlApi;
+    this.myChannels$ = this.httpClient.get<Canal[]>(
+      `${this.urlApi}/servers/canaux/${this.usersService.userId}`
+    );
+    this.channels$ = this.httpClient.get<Canal[]>(`${this.urlApi}/canaux`);
+    this.selectedChannel = new Subject<Canal>();
+
+    this.listMessages$ = this.httpClient.get<Message[]>(`${this.urlApi}/messages/`);
+  }
+
+  public selectChannel(channel: Canal): void {
+    this.selectedChannel.next(channel);
+  }
 }

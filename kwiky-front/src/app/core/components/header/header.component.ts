45c29259c7b97d960b62225af1905d5d54049a73
PierreGau <p.gauthier09@gmail.com>
2022-12-22 22:16:07 +0100

    Correctif affichage user dans header

--- kwiky-front/src/app/core/components/header/header.component.ts
+++ kwiky-front/src/app/core/components/header/header.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../../models/user';
 import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  public user!: User;
+  public user$!: Observable<User>;
 
-  constructor(private usersService: UsersService){
-    this.usersService.currentUser$.subscribe((data) => (this.user = data))
+  constructor(private usersService: UsersService) {
+    this.user$ = this.usersService.currentUser$;
   }
 }
